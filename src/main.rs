34376bc6674703f4d6ceb2156420335c5cadca93
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-01 13:33:19 -0600

    understanding shortcut syntax with structs when dealing with paramaters and variables.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,22 @@ struct Coffee {
 fn main() {
     let name: String = String::from("Mocha");
     let coffee: Coffee = make_coffee(name, 5.99, true);
+    println!("{}, {}, {}", coffee.name, coffee.price, coffee.is_hot);
+
+    let name: String = String::from("Latte");
+    let price: f64 = 3.99;
+    let is_hot: bool = false;
+    let latte: Coffee = Coffee {
+        name,
+        price,
+        is_hot,
+    };
 
     fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
         Coffee {
-            name: name,
-            price: price,
-            is_hot: is_hot,
+            name,
+            price,
+            is_hot,
         }
     }
-    println!("{}, {}, {}", coffee.name, coffee.price, coffee.is_hot)
 }

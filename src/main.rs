73c1ccd9b3ea4515287a362149e777a3e749f500
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-01 14:47:07 -0600

    understanding what would be required to fully clone a struct that contains data types that do not impliment the copy trait.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,19 @@ struct Coffee {
     is_hot: bool,
 }
 
-fn main() {
-    let name: String = String::from("Mocha");
-    let coffee: Coffee = make_coffee(name, 5.99, true);
-    println!("{}, {}, {}", coffee.name, coffee.price, coffee.is_hot);
-
-    let name: String = String::from("Latte");
-    let price: f64 = 3.99;
-    let is_hot: bool = false;
-    let latte: Coffee = Coffee {
+fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
+    Coffee {
         name,
         price,
         is_hot,
-    };
-
-    fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
-        Coffee {
-            name,
-            price,
-            is_hot,
-        }
     }
+}
+
+fn main() {
+    let mocha: Coffee = make_coffee(String::from("Mocha"), 4.99, true);
+
+    let frapachino: Coffee = Coffee {
+        name: String::from("{Frapachino}"),
+        ..mocha
+    };
 }

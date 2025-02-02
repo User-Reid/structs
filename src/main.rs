3503d59c3c59b9af0209293d88db839c7eb7f158
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-02 10:45:19 -0600

    understanding the different ways that you can impliment the self parameter.

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,29 @@ struct TaylorSwiftSong {
 }
 
 impl TaylorSwiftSong {
-    fn display_song_info(self: Self) {
-        // Immutable struct value (self param takes ownership)
+    // Immutable struct value (self param takes ownership)
+    // -> Immutable reference to the struct instance (no ownership moved)
+    fn display_song_info(&self) {
         println!("Title: {}", self.title);
         println!("Release Year {}", self.release_year);
         println!("Duration: {} seconds", self.duration_secs);
     }
 
     // Mutable struct value (self parameter takes ownership, has permission to mutate)
-    fn double_length(mut self) {
-        self.duration_secs = self.duration_secs * 2;
-        println!("{:#?}", self)
-    }
-
-    // Immutable reference to the struct instance (no ownership moved)
     // Mutable reference to the struct instance (no ownership moved, have permission to mutate)
+    fn double_length(&mut self) {
+        self.duration_secs = self.duration_secs * 2;
+    }
 }
 
 fn main() {
-    let song: TaylorSwiftSong = TaylorSwiftSong {
+    let mut song: TaylorSwiftSong = TaylorSwiftSong {
         title: String::from("Blank Space"),
         release_year: 2014,
         duration_secs: 231,
     };
 
+    song.display_song_info();
     song.double_length();
+    song.display_song_info();
 }

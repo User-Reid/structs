4367eb20a68e3c165fbf43ae9ad5407f0483e5bc
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-02 19:28:08 -0600

    understanding what a unit struct type is.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-// Hours, Minutes
-struct ShortDuration(u32, u32);
-
-// Years, Months
-struct LongDuration(u32, u32);
+struct Empty;
 
 fn main() {
-    let work_shift: ShortDuration = ShortDuration(8, 0);
-    let era: LongDuration = LongDuration(5, 3);
-    println!("{} hours, {} minutes", work_shift.0, work_shift.1);
-    println!("{} years, {} months", era.0, era.1);
+    let my_empty_struct: Empty = Empty;
 }

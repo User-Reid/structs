9ff49f769dd1f41f725dbf25eac5968438624bc0
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-02 18:57:32 -0600

    understanding builder pattern within design patterns as well as understanding tuple structs and their importance for orginization and bug prevention.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,12 @@
-#[derive(Debug)]
-struct TaylorSwiftSong {
-    title: String,
-    release_year: u32,
-    duration_secs: u32,
-}
+// Hours, Minutes
+struct ShortDuration(u32, u32);
 
-impl TaylorSwiftSong {
-    fn new(title: String, release_year: u32, duration_secs: u32) -> Self {
-        Self {
-            title,
-            release_year,
-            duration_secs,
-        }
-    }
-}
-
-impl TaylorSwiftSong {
-    fn display_song_info(&self) {
-        println!("Title: {}", self.title);
-        println!("Years since release: {}", self.years_since_release());
-        println!("Duration: {} seconds", self.duration_secs);
-    }
-
-    fn double_length(&mut self) {
-        self.duration_secs = self.duration_secs * 2;
-    }
-
-    fn is_longer_than(&self, x: &Self) -> bool {
-        self.duration_secs > x.duration_secs
-    }
-
-    fn years_since_release(&self) -> u32 {
-        2024 - &self.release_year
-    }
-}
+// Years, Months
+struct LongDuration(u32, u32);
 
 fn main() {
-    let blank_space = TaylorSwiftSong::new(String::from("Bad Bitch"), 2024, 399);
-
-    let all_too_well: TaylorSwiftSong = TaylorSwiftSong {
-        title: String::from("All Too Well"),
-        release_year: 2014,
-        duration_secs: 327,
-    };
-
-    if blank_space.is_longer_than(&all_too_well) {
-        println!(
-            "{} is longer than {}",
-            blank_space.title, all_too_well.title
-        )
-    } else {
-        println!(
-            "{} is longer than {}",
-            all_too_well.title, blank_space.title
-        )
-    }
-
-    all_too_well.display_song_info();
-    blank_space.display_song_info();
+    let work_shift: ShortDuration = ShortDuration(8, 0);
+    let era: LongDuration = LongDuration(5, 3);
+    println!("{} hours, {} minutes", work_shift.0, work_shift.1);
+    println!("{} years, {} months", era.0, era.1);
 }
